0fa0af7e8bc2fb1027505ff936009d580231bd6c
Zu3zz <zthzththth@163.com>
2020-04-20 17:59:14 +0800

    feat: finish verification code

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.nowcoder.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
 import org.slf4j.Logger;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 import static com.nowcoder.community.util.CommunityConstant.ACTIVATION_REPEAT;
@@ -28,6 +35,8 @@ public class LoginController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private Producer kaptchaProducer;
 
     @Value("${server.servlet.context-path}")
     private String contextPath;
@@ -38,7 +47,7 @@ public class LoginController {
     }
 
     @GetMapping(path = "/login")
-    public String getLoginPage(){
+    public String getLoginPage() {
         return "site/login";
     }
 
@@ -61,24 +70,43 @@ public class LoginController {
      * 用户激活的逻辑
      * url路径： http://localhost:8080/community/activation/userId/code
      *
-     * @param model 给前端页面模板传参
+     * @param model  给前端页面模板传参
      * @param userId userId
-     * @param code 用户激活的code
+     * @param code   用户激活的code
      * @return 返回给前端模板需要的数据
      */
     @GetMapping("/activation/{userId}/{code}")
     public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code) {
-        int result = userService.activation(userId,code);
-        if(result == ACTIVATION_SUCCESS){
-            model.addAttribute("msg","注册成功，我们已经向您的邮箱发送了一封激活邮件，请尽快激活！");
-            model.addAttribute("target","/login");
-        } else if(result == ACTIVATION_REPEAT){
-            model.addAttribute("msg","无效操作，该账号已经激活过了！");
-            model.addAttribute("target","/index");
+        int result = userService.activation(userId, code);
+        if (result == ACTIVATION_SUCCESS) {
+            model.addAttribute("msg", "注册成功，我们已经向您的邮箱发送了一封激活邮件，请尽快激活！");
+            model.addAttribute("target", "/login");
+        } else if (result == ACTIVATION_REPEAT) {
+            model.addAttribute("msg", "无效操作，该账号已经激活过了！");
+            model.addAttribute("target", "/index");
         } else {
-            model.addAttribute("msg","激活失败，激活码错误");
-            model.addAttribute("target","/index");
+            model.addAttribute("msg", "激活失败，激活码错误");
+            model.addAttribute("target", "/index");
         }
         return "site/operate-result";
+    }
+
+    @GetMapping("/kaptcha")
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        // 生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        // 将验证码存入session
+        session.setAttribute("kaptcha",text);
+        // 将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image,"png",os);
+        } catch (IOException e) {
+            logger.error("相应验证码失败" + e.getMessage());
+        }
+
     }
 }
